7a5a3eae632ed791852f303c88ce27bdcf4c59a9
chrisn44 <chrisn28@yahoo.com>
2026-03-07 16:23:52 +0200

    Update bot_handlers_start.py

--- bot_handlers_start.py
+++ bot_handlers_start.py
@@ -1,7 +1,6 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from bot_database import get_db
-from bot_models import User
 from bot_utils_helpers import get_user
 
 ZODIAC_SIGNS = [
@@ -11,17 +10,15 @@ ZODIAC_SIGNS = [
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
-    db_gen = get_db()
-    db = await db_gen.__anext__()
+    db = await get_db()
 
-    db_user = await get_user(db, user.id)
+    # Get or create user
+    db_user = db.get_user(user.id)
     if not db_user:
-        db_user = User(telegram_id=user.id, username=user.username)
-        db.add(db_user)
-        await db.commit()
+        db_user = db.create_user(user.id, user.username)
 
-    if db_user.sign:
-        sign_name = ZODIAC_SIGNS[db_user.sign-1]
+    if db_user.get('sign'):
+        sign_name = ZODIAC_SIGNS[db_user['sign']-1]
         await update.message.reply_text(
             f"✨ Welcome back, {user.first_name}!\n"
             f"Your sign is **{sign_name}**.\n\n"
@@ -38,7 +35,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "🌙 Welcome to LunarSignsBot!\n\nPlease select your zodiac sign:",
             reply_markup=reply_markup
         )
-    await db_gen.aclose()
 
 async def set_sign_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
@@ -46,12 +42,8 @@ async def set_sign_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     data = query.data
     if data.startswith("set_sign_"):
         sign = int(data.split("_")[2])
-        db_gen = get_db()
-        db = await db_gen.__anext__()
-        user = await get_user(db, update.effective_user.id)
-        user.sign = sign
-        await db.commit()
-        await db_gen.aclose()
+        db = await get_db()
+        db.update_user(update.effective_user.id, sign=sign)
         await query.edit_message_text(
             f"Great! Your sign is set to **{ZODIAC_SIGNS[sign-1]}**.\n\n"
             "Now you can use /horoscope to see what the stars have in store.",
